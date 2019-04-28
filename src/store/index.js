7bfbfa0d2569674dfcafb39e221a05ea3d6ae1da
webclidev <devkant.dhruv@gmail.com>
2019-04-28 15:50:23 +0530

    added vuexPathify

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import pathify from './pathify' //calling pathify config file which we created 
 import store from './modules/store'
 import cart from './modules/cart'
 
 Vue.use(Vuex)
 
-const debug = process.env.NODE_ENV !== 'production'
-
 export default new Vuex.Store({
+  plugins: [
+    pathify.plugin
+  ],
   modules: {
     store,
     cart
-  },
-  strict: debug,
+  }
 })
--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,20 +1,10 @@
+import { make } from 'vuex-pathify'
 const state = {
   products: 'lifeBuoy'
 }
-const getters = {
-  showProduct: state => state
-}
-const mutations = {
-  updateProduct(state, payload) {
-    state.products = payload
-  }
-}
-const actions = {
-}
+const mutations = make.mutations(state)
 export default {
   namespaced: true,
   state,
-  mutations,
-  actions,
-  getters
+  mutations
 }
